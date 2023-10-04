056dd165575fa87f468811941cc4d2441bbfab00
Utkarsh <hauntedutkarsh@gmail.com>
2023-10-04 22:30:21 +0530

    build navbar //non functional

--- App.js
+++ App.js
@@ -1,2 +1,66 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+
+/* Basic layout of website- 
+
+Header
+   -logo
+   -nav items
+
+Body
+   -Search
+   -Restraunt Container
+   -Restraunt Card
+
+Footer
+   -Copyright
+   -links
+   -Address
+   -Contact
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="brand">
+        <img
+          className="brand_logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx3d5OTRg8jgtxX22MoEYUWeP4haZ1K-GZoKqM3fb9i1quzE6wBvLI--RqegXHVYbKCkQ&usqp=CAU"
+        />
+        <h1 className="brand_name">Baratie</h1>
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        Search
+      </div>
+      <div className="res-container"></div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
