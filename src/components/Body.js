fc024063013c0fc0b003171bcd467182e4eeae91
Utkarsh <hauntedutkarsh@gmail.com>
2023-10-06 17:47:11 +0530

    integrated swiggy api to fetch data using hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import resList from "../../utils/mockData";
 
 const Body = () => {
   //local state variable - very powerful state variable
   const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4697247&lng=77.4956607&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    //converting this fetch data to json
+    const json = await data.json();
+
+    console.log(json);
+    setListOfRestaurant(
+      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+    );
+    // setListOfRestaurant(json.data.card[2].data);
+  };
+
   return (
     <div className="body">
       <div className="filter">
