eeda65d990490065351f340b9bf61984c95f89e0
Utkarsh <hauntedutkarsh@gmail.com>
2023-10-07 22:46:02 +0530

    added link component from react-router-dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { LOGO_URL } from "../../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnName, setbtnName] = useState("Login");
@@ -7,10 +8,6 @@ const Header = () => {
 
   //if no dependency array => useEffect is called on every render
   //if dependency array is empty = [] => useEffect is called on initial render(just once)
-
-  useEffect( () => {
-    console.log("useEffect called");
-  }, [btnName]);
 
   return (
     <div className="header">
@@ -24,10 +21,20 @@ const Header = () => {
 
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/cart">Cart</Link>
+          </li>
           <button
             className="login-btn"
             onClick={() => {
