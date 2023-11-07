cffaf9c57ae354a310fd2605f1f7ed7a81075412
Utkarsh <hauntedutkarsh@gmail.com>
2023-11-08 00:39:14 +0530

    made banner for the project

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,9 @@ const Header = () => {
   //if no dependency array => useEffect is called on every render
   //if dependency array is empty = [] => useEffect is called on initial render(just once)
 
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
   return (
     <div className="contain">
       <div className="header">
@@ -48,6 +51,54 @@ const Header = () => {
               {btnName}{" "}
             </button>
           </ul>
+        </div>
+      </div>
+      <div className="banner-wrapper">
+        <h1 className="title">Baratie</h1>
+        <h2 className="title-desc">
+          We understand starving people more than anyone
+        </h2>
+        <div className="search-bar">
+          <div className="filter">
+            <div className="search">
+              <input
+                type="text"
+                className="search-box"
+                value={searchText}
+                onChange={(e) => {
+                  setSearchText(e.target.value);
+                }}
+              ></input>
+              <button
+                className="search-btn"
+                onClick={() => {
+                  //filter the restaurant cards and update the UI
+                  //searchText
+
+                  const filterRestaurant = listOfRestaurants.filter((res) =>
+                    res.info.name
+                      .toLowerCase()
+                      .includes(searchText.toLowerCase())
+                  );
+
+                  setfilterRestaurant(filterRestaurant);
+                }}
+              >
+                Search
+              </button>
+            </div>
+            <button
+              className="filter-btn"
+              onClick={() => {
+                const filteredList = listOfRestaurants.filter(
+                  (res) => res.info.avgRating > 4.2
+                );
+                setListOfRestaurant(filteredList);
+              }}
+            >
+              Top Rated Restaurant
+            </button>
+          </div>
         </div>
       </div>
     </div>
